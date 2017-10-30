4bc7ff64cee11ed350705e7bfc0f0960df273340
John Daly <jpdaly@gmail.com>
2017-10-30 06:23:39 +0000

    some improvements to 37, still no working but :)

--- euler37.py
+++ euler37.py
@@ -6,12 +6,13 @@ def isPrime(o):
 
     n = int(o)
 
-    if n in primes:
+    if n < primes[-1] and n in primes:
         return True
+    elif n < primes[-1] and n not in primes:
+        return False
 
     if n == 1:
         return False
-
     
     for p in primes:
         if n % p == 0:
@@ -22,43 +23,50 @@ def isPrime(o):
 
 
 def l_to_r_truncate(n):
-    print n
+    if len(n) == 1 and isPrime(n):
+        return True
+
     if not isPrime(n):
         return False
 
     if not len(n) == 1:
-        l_to_r_truncate(n[-(len(n) - 1):])
-    else:
-        isPrime(n)
+        return l_to_r_truncate(n[-(len(n) - 1):])
 
 
 def r_to_l_truncate(n):
     if len(n) == 1:
         return isPrime(n)
     elif isPrime(n):
-        r_to_l_truncate(n[:-1])
+        return r_to_l_truncate(n[:-1])
     else:
         return False
 
 
-
 def get_the_answer():
     i = 8
-    while len(truncatables) < 1:
+    while len(truncatables) < 11:
         print i
         if isPrime(i):
-            print l_to_r_truncate(str(i))
-            print r_to_l_truncate(str(i))
-            if l_to_r_truncate(str(i)) and r_to_l_truncate(str(i)):
-                truncatables.append(i)
+            if check_initial_digits(i):
+                if l_to_r_truncate(str(i)) and r_to_l_truncate(str(i)):
+                    truncatables.append(i)
 
         i += 1
 
+        print truncatables
     print truncatables
 
+
+def check_initial_digits(n):
+    first = get_digit_at_positions(n, 0)
+    last = get_digit_at_positions(n, -1)
+    return first in primes and last in primes
+
+def get_digit_at_positions(n, position):
+    return int(str(n)[position])
+
 for n in range(1, 4000):
-    isPrime(n)
+    print ":" + str(n) + " " + str(isPrime(n))
 
-print primes
 
-print r_to_l_truncate('23')
+get_the_answer()
